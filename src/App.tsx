fb1f9e4c48a799af7bc54f360a4334de6c09ff19
jurekg <jurekgz27@gmail.com>
2025-09-23 14:04:33 +0200

    first

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const BASE_URL = "https://www.fruityvice.com/api/fruit";
+
+function App() {
+
+}
+
+export default App
