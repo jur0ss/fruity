b2b775ede747b920f598af93354c2ee3ecbf65e9
jurekg <jurekgz27@gmail.com>
2025-09-23 14:19:13 +0200

    Imported fruit api and started working on fetching data

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {useEffect, useState} from "react";
 
 const BASE_URL = "https://www.fruityvice.com/api/fruit";
 
+type Fruit = {
+    name: string;
+    carbohydrates: number;
+    fat: number;
+    protein: number;
+    calories: number;
+    sugar: number;
+}
+
 function App() {
+
+    const [fruits, setFruits] = useState<Fruit[]>([]);
+
+    const fetchData = async () => {
+
+        const getAllFruits = () => {
+            fetch(`${BASE_URL}/all`)
+                .then (data => data.json())
+                .then (jsonData => setFruits(jsonData))
+                .catch(error => console.log(error));
+        }
+
+
+    }
 
 }
 
